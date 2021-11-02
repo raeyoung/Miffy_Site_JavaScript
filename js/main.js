1f3941a84c696b5a21f5b35805af775ce7d01646
raeyoung <raeyoung233@gmail.com>
2021-11-02 16:36:39 +0900

    링크 생성 / 이름 변경

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
 
